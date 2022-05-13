22e5ac3e3c3c19031187f4b092c30cc035c272e9
Muhammad Abdullah Uppal <59197202+Abdullah-Uppal@users.noreply.github.com>
2022-05-13 21:05:41 +0500

    New: Recursive Binary Search

--- Algorithms/Searching/binarysearch.cpp
+++ Algorithms/Searching/binarysearch.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 
 int binarysearch(int element, int arr[], int n);
+int recursivebinarysearch(int element, int arr[], int start, int end);
 
 int main() {
     const int SIZE = 10;
     int arr[SIZE] = {1, 2, 4, 6, 23, 34, 43, 54, 456, 564};
-    int index = binarysearch(456, arr, SIZE);
+    int tosearch = 1;
+    int index = recursivebinarysearch(tosearch, arr, 0, SIZE-1);
+    std::cout << "(recursive) ";
     if (index > -1)
-        std::cout << "Element 456 found at: " << index << std::endl;
+        std::cout << "Element " << tosearch <<  " found at: " << index << std::endl;
     else
         std::cout << "Element not found\n";  
+
+    tosearch = 54;
+    index = binarysearch(tosearch, arr, SIZE);
+    std::cout << "(iterative) ";
+    if (index > -1)
+        std::cout << "Element " << tosearch <<  " found at: " << index << std::endl;
+    else
+        std::cout << "Element not found\n";
     return 0;
 }
 
@@ -29,4 +40,18 @@ int binarysearch(int element, int arr[], int n) {
         }
     }    
     return -1;
+}
+
+int recursivebinarysearch(int element, int arr[], int start, int end) {
+    if (start>end) {
+        return -1;
+    }
+    int mid = (start+end)/2;
+    if (arr[mid] == element) {
+        return mid;
+    }
+    else if (arr[mid] < element) {
+        return recursivebinarysearch(element, arr, mid+1, end);
+    }
+    return recursivebinarysearch(element, arr, start, mid-1);
 }
