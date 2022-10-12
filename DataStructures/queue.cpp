5a7f505b72acffda9a5873ec857859cc02909f20
Muhammad Khuzaima Umair <mkhuzaimaumair@gmail.com>
2022-10-12 21:34:41 +0500

    Queue DS contructors, destructors, clear, size methods

--- DataStructures/queue.cpp
+++ DataStructures/queue.cpp
@@ -6,6 +6,11 @@ class Node {
 public:
     int data;
     Node* next;
+    // constructors
+    Node() { data = 0; next = NULL; }
+    Node(int d) { data = d; next = NULL; }
+    Node(int d, Node* n) { data = d; next = n; }
+    ~Node() { }
 };
 
 // Stack class
@@ -13,48 +18,69 @@ class Queue {
 private:
     Node* front;
     Node* rear;
+    int elementCount;
 
 public:
-    Queue() { front = rear = NULL; }
+    Queue() { front = rear = NULL; elementCount = 0; }
+    // copy constructor
+    Queue(const Queue& q);
     ~Queue();
     void enqueue(int x);
     int dequeue();
     void Display();
     bool isEmpty();
+    int size();
+    void clear();
 };
+
+// copy constructor; deep copy
+Queue::Queue(const Queue& q) {
+    
+    Node *p = q.front;
+    Node *n = NULL;
+    front = rear = NULL;
+    while (p) {
+        n = new Node(p->data);
+        // if it is the first node, update top
+        if (rear == NULL) {
+            front = rear = n;
+        } else {
+            // add at end
+            rear->next = n;
+        }
+        // update tail/rear
+        rear = n;
+        p = p->next;
+    }
+
+    elementCount = q.elementCount;
+}
 
 // Destructor
 Queue::~Queue() {
-    Node* p = front;
-    while (front) {
-        front = front->next;
-        delete p;
-        p = front;
-    }
+    clear();
 }
 
 // isEmpty() function
 bool Queue::isEmpty() {
-    if (front == NULL)
-        return true;
-    else
-        return false;
+    return elementCount == 0;
+    // or 
+    // return front == NULL;
 }
 
 // enqueue function
 void Queue::enqueue(int x) {
-    Node* t = new Node;
+    Node* t = new Node (x);
     if (t == NULL)
         cout << "Queue is full" << endl;
     else {
-        t->data = x;
-        t->next = NULL;
         if (front == NULL)
             front = rear = t;
         else {
             rear->next = t;
             rear = t;
         }
+        ++elementCount;
     }
 }
 
@@ -68,6 +94,7 @@ int Queue::dequeue() {
         Node* t = front;
         front = front->next;
         delete t;
+        --elementCount;
     }
     return x;
 }
@@ -82,6 +109,22 @@ void Queue::Display() {
     cout << endl;
 }
 
+int Queue::size() {
+    return elementCount;
+}
+
+// clear function
+void Queue::clear() {
+    Node* p = front;
+    while (front) {
+        front = front->next;
+        delete p;
+        p = front;
+    }
+    front = rear = NULL;
+    elementCount = 0;
+}
+
 /**
 // Main function
 int main() {
@@ -93,6 +136,12 @@ int main() {
     cout<<"Queue 30"<<endl;
     q.enqueue(30);
 
+    cout<<"Display Queue 1"<<endl;
+    q.Display();
+    Queue q2 = q;
+    cout<<"Display Queue 2"<<endl;
+    q2.Display();
+
     cout<<"Popping elements from the stack "<<endl;
 
     while (!q.isEmpty()) {
@@ -100,4 +149,4 @@ int main() {
     }
     return 0;
 }
-**/
+// **/
